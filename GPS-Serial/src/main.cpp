3fb4b4c5c4174f2cd63f83883ac6f6062bd1809b
Louis Bertrand <louis@bertrandtech.ca>
2023-07-09 07:59:13 -0400

    Works with breadboard

---
+++ GPS-Serial/src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <TinyGPS++.h>
+
+constexpr uint32_t GPSBaud = 9600;
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+void displayInfo(void);
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(GPSBaud);
+  Serial.println("Hello GPS Test");
+}
+
+void loop() {
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial1.available() > 0)
+    gps.encode(Serial1.read());
+    displayInfo();
+    // Serial.print("Satellites: "); Serial.println(gps.satellites.value());
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
